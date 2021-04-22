ae9b46921a5c231cd5a82de74e7750654cea329c
Chris <resavestation@gmail.com>
2021-04-22 18:42:25 +0800

    1. 增加端口輸入資料 2. 繪製SVG Style待寫

--- src/components/SVGSource/PieChartPercent.jsx
+++ src/components/SVGSource/PieChartPercent.jsx
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./PieChartPercent.sass";
 
 const PieChartPercent = (props) => {
+  // 輸入數字資料及顏色資料，產生圓餅圖
+  const pieData = props.pieData ? props.pieData : [];
+  const drawPieChart = async () => {
+    const pieItem = document.getElementsByClassName("pieChart__item");
+    if (pieData.length > 0) {
+      const sumData = pieData.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.number;
+      }, 0);
+      let dashoffset = 0;
+      for (let i in pieData) {
+        const circle = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "circle"
+        );
+        circle.setAttribute("r", 16);
+        circle.setAttribute("cx", 16);
+        circle.setAttribute("cy", 16);
+        circle.setAttribute = ("class", "pieChart__circle");
+        const circleStyle = `stroke: ${pieData[i].color}; stroke-dasharray: ${
+          (pieData[i].number / sumData) * 100
+        } 100; stroke-dashoffset: ${dashoffset}`;
+        dashoffset = dashoffset - (pieData[i].number / sumData) * 100;
+        //circle.setAttribute("style", circleStyle);
+        pieItem[0].appendChild(circle);
+      }
+      //document.body.appendChild(pieList);
+    }
+  };
+  useEffect(drawPieChart, []);
   return (
     <div className="pieChart">
       <div className="pieChart__wrapper">
-        <svg className="pieChart__list" viewBox="0 0 32 32">
-          <circle
-            className="pieChart__circle pieChart__circle--1"
-            r="16"
-            cx="16"
-            cy="16"
-          />
-          <circle
-            className="pieChart__circle pieChart__circle--2"
-            r="16"
-            cx="16"
-            cy="16"
-          />
-          <circle
-            className="pieChart__circle pieChart__circle--3"
-            r="16"
-            cx="16"
-            cy="16"
-          />
-          <circle
-            className="pieChart__circle pieChart__circle--4"
-            r="16"
-            cx="16"
-            cy="16"
-          />
+        <svg className="pieChart__item" viewBox="0 0 32 32">
+          <circle className="pieChart__circle" r="16" cx="16" cy="16" />
         </svg>
       </div>
     </div>
--- src/components/SVGSource/index.jsx
+++ src/components/SVGSource/index.jsx
@@ -4,6 +4,34 @@ import PieChartSingle from "./PieChartSingle";
 import PieChartPercent from "./PieChartPercent";
 
 const SVGSource = () => {
+  const pieData = [
+    {
+      color: "#8bfdfc",
+      number: 1,
+    },
+    {
+      color: "#46a2f7",
+      number: 1,
+    },
+    {
+      color: "#3f74f7",
+      number: 1,
+    },
+    {
+      color: "#3e44ff",
+      number: 1,
+    },
+    {
+      color: "#127e41",
+      number: 1,
+    },
+  ];
+  /**
+   * var reducePlus = pieData.reduce((accumulator, currentValue) => {
+  // 分別為前一個回傳值, 目前值, 當前索引值
+  return accumulator + currentValue.nubmer;  // 與前一個值相加
+}, 0);              
+   */
   return (
     <div className="source">
       <div className="source__wrapper">
@@ -15,7 +43,7 @@ const SVGSource = () => {
           <Col span={24}>
             {/* 修改成會自己長成框框寬度100%，所以測試時寫死一個寬 */}
             <div style={{ width: "250px", marginTop: "5%" }}>
-              <PieChartPercent />
+              <PieChartPercent pieData={pieData} />
             </div>
           </Col>
         </Row>
